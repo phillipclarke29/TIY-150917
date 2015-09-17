86ddbc6ef0be10f5edeaee583347e6976a93af0d
philip <philip.clarke@redhood.co.uk>
2015-09-17 20:23:48 +0100

    changed rand 3 to 4

--- game.rb
+++ game.rb
@@ -1,1 +1,19 @@
+choices = ['r','p','s']
+
+
 puts "welcome to Rock Paper Scissors!!!"
+
+puts "Please enter your name"
+name = gets.chomp
+puts  "Please make your choice r,p,s"
+players_choice = gets.chomp
+
+computer_choice_index = rand(4)
+computer_choice = choices[computer_choice_index]
+puts computer_choice
+
+if (players_choice == "s")
+  puts "Computer Wins"
+elsif (players_choice == 'p')
+  puts "you win"
+end
